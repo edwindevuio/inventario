e8e09d8dd4721ec6a242075aadc784c9e56bc78d
Edwin Maila <edwindevuio@gmail.com>
2023-08-26 14:05:21 -0500

    chan2681

--- index.php
+++ index.php
@@ -3,6 +3,28 @@
   require_once('includes/load.php');
   if($session->isUserLoggedIn(true)) { redirect('home.php', false);}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tu título aquí</title>
+    <link rel="stylesheet" href="ruta/a/tu/hoja/de/estilos.css"> <!-- Enlaza tu hoja de estilos si la tienes -->
+    <style>
+        /* Estilo para la imagen de fondo */
+        .login-page {
+            background-image: url('ruta/a/tu/imagen/de/fondo.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            min-height: 100vh; /* Asegura que la imagen de fondo cubra toda la pantalla */
+        }
+        /* Resto de estilos que ya tienes */
+    </style>
+</head>
+<body>
+    <!-- Tu contenido HTML y PHP aquí -->
+    
 <?php include_once('layouts/header.php'); ?>
 <div class="login-page">
     <div class="text-center">
@@ -25,3 +47,12 @@
     </form>
 </div>
 <?php include_once('layouts/footer.php'); ?>
+
+</body>
+</html>
+
+
+
+
+
+
